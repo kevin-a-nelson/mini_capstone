cacc8ab55f35b692515d07fcee48386bac9c1165
Kevin <nelson67@stolaf.edu>
2019-06-30 13:21:46 -0500

    crud working

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,11 +1,39 @@
 class Api::ProductsController < ApplicationController
-  def product
-    @product = Product.first
-    render 'product.json.jb'
+  def index
+    @products = Product.all
+    render 'index.json.jb'
   end
 
-  def all_products
-    @products = Product.all
-    render 'all_products.json.jb'
+  def show
+    @product = Product.all.find_by(id: params['id'])
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description
+    )
+    render 'show.json.jb'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render 'show.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get '/product' => 'products#product'
-    get '/all_products' => 'products#all_products'
+    get '/products' => 'products#index'
+    get '/products/:id' => 'products#show'
+    post '/products' => 'products#create'
+    patch '/products/:id' => 'products#update'
+    delete '/products/:id' => 'products#destroy'
   end
 end
